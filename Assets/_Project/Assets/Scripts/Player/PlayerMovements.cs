a7a20a83f576a180094064d5845a103f1b23d1b3
Enzo Bossé <bosseenzo6@gmail.com>
2026-02-10 21:55:13 +0100

    [ADD] PlayerMovements

--- Assets/_Project/Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/_Project/Assets/Scripts/Player/PlayerMovements.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovements : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _speedScaler;
+    [SerializeField] private float _speedIdle = 1.25f;
     private Vector2 _currentInput;
     private Vector2 _lastDirection;
+    private bool _isMoving = false;
 
     public void OnPlayerMove(InputAction.CallbackContext context)
     {
@@ -14,19 +19,33 @@ public class PlayerMovements : MonoBehaviour
         if (_currentInput.sqrMagnitude > 0.01f)
         {
             _lastDirection = _currentInput;
-            UpdateAnimator(_currentInput, true);
+            _isMoving = true;
+            _animator.speed = _speedScaler / 2;
         }
         else
         {
-            UpdateAnimator(_lastDirection, false);
+            _isMoving = false;
+            _animator.speed = _speedIdle;
+        }
+        UpdateAnimator();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isMoving)
+        {
+            _rb.MovePosition(_rb.position + _currentInput * _speedScaler * Time.fixedDeltaTime);
         }
     }
 
-    private void UpdateAnimator(Vector2 dir, bool isMoving)
+    private void UpdateAnimator()
     {
         if(_animator == null) return;
+
+        Vector2 dir = _isMoving ? _currentInput : _lastDirection;
+        
         _animator.SetFloat("Dir_x", dir.x);
         _animator.SetFloat("Dir_y", dir.y);
-        _animator.SetBool("IsMoving", isMoving);
+        _animator.SetBool("IsMoving", _isMoving);
     }
 }
