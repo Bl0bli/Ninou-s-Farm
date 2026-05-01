77871e4a1b555159d5e4f8003029444e4ffb849a
Enzo Bossé <bosseenzo6@gmail.com>
2026-05-01 16:07:49 +0200

    [ADD] Idle ANime For Items

--- Assets/_Project/Assets/Scripts/Items/ItemPickable.cs
+++ Assets/_Project/Assets/Scripts/Items/ItemPickable.cs
@@ -1,16 +1,24 @@
-using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ItemPickable : MonoBehaviour, ICollectible
 {
     [SerializeField] private SpriteRenderer _renderer;
-    [SerializeField] UnityEvent OnPickup;
-    [SerializeField] ItemData _itemData;
+    [SerializeField] private UnityEvent OnPickup;
+    [SerializeField] private ItemData _itemData;
+
+    [Header("Idle Floating Animation")]
+    [SerializeField] private float _floatHeight = 0.15f;
+    [SerializeField] private float _floatDuration = 1.2f;
+
+    private Tween _idleTween;
+    private Vector3 _initialLocalPosition;
 
     private void Start()
     {
         SetupItemPickable();
+        StartIdleFloatingAnimation();
     }
 
     public void Init(ItemData item)
@@ -23,18 +31,39 @@ public class ItemPickable : MonoBehaviour, ICollectible
     {
         if (_itemData != null)
         {
-            if(_renderer == null)_renderer = GetComponent<SpriteRenderer>();
+            if (_renderer == null)
+                _renderer = GetComponent<SpriteRenderer>();
+
             _renderer.sprite = _itemData.WorldSprite;
         }
     }
-    
+
+    private void StartIdleFloatingAnimation()
+    {
+        _initialLocalPosition = transform.localPosition;
+
+        _idleTween?.Kill();
+
+        _idleTween = transform
+            .DOLocalMoveY(_initialLocalPosition.y + _floatHeight, _floatDuration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetLink(gameObject);
+    }
+
     public void Collect(PlayerInventory inventory)
     {
         if (inventory.PickUp(_itemData))
         {
             OnPickup?.Invoke();
-            
+
+            _idleTween?.Kill();
             Destroy(gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        _idleTween?.Kill();
     }
 }
